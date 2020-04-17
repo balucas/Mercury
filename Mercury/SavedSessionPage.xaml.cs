3a076521a9ca619337e6b5b22fcf3082574fbd56
Bernard Lucas <blucas1597@gmail.com>
2020-04-17 02:46:12 -0400

    implemented multi-select saved session

--- Mercury/SavedSessionPage.xaml.cs
+++ Mercury/SavedSessionPage.xaml.cs
@@ -8,12 +8,14 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Diagnostics;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -26,20 +28,35 @@ namespace Mercury
 
     public sealed partial class SavedSessionPage : Page
     {
-        public ObservableCollection<AudienceFrame> FaceData { get; }
+        public List<SavedSession> SavedSessions { get; }
 
         public SavedSessionPage()
         {
             this.InitializeComponent();
-            FaceData = new ObservableCollection<AudienceFrame>();
+            SavedSessions = Session.RetrieveSavedSessions(Windows.Storage.ApplicationData.Current.LocalFolder.Path);
         }
 
         private void Page_Loaded(object sender, RoutedEventArgs e)
         {
-
+            //SessionList.SelectedItems;
         }
+
+
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
+        }
+
+        private void View_Stats(object sender, RoutedEventArgs e)
+        {
+            var items = SessionList.SelectedItems;
+            var selected = new List<SavedSession>();
+            foreach(var s in items)
+            {
+                selected.Add(s as SavedSession);
+            }
+
+            //Navigate to stats page and pass selected sessions
+            Frame.Navigate(typeof(StatisticsPage), selected);
         }
     }
 }
